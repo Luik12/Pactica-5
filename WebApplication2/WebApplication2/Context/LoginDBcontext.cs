d7951506c7f164730231541673c9581636f63df3
Luik12 <luis.pena.inpha@gmail.com>
2026-02-27 21:42:30 -0400

    pi

---
+++ WebApplication2/WebApplication2/Context/LoginDBcontext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebApplication2.Modelo;
+
+namespace WebApplication2.Context
+{
+    public class LoginDBcontext : DbContext
+    {
+        public LoginDBcontext(DbContextOptions<LoginDBcontext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>()
+                .HasIndex(u => u.Correo)
+                .IsUnique();
+        }
+    }
+    
+   
+
+}
---
+++ WebApplication2/WebApplication2/Modelo/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Modelo
+{
+    public class Usuario
+    {
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        public DateTime FechaDeNacimiento { get; set; }
+    }
+}
