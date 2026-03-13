77f0b0718f129fe758abccb8798ed89ffe3ceb6d
Luik12 <luis.pena.inpha@gmail.com>
2026-03-13 18:41:20 -0400

    unyimaact

--- WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce870cd67d017acf6b5680276426f743b5e277a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d422378d66bbfe96143d92c5b9c1ceaac8ce5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
